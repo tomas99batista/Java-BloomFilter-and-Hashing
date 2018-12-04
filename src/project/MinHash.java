e424b129caac3ca9efd9f4d0c472d9b8cd52277b
santorfo <39157156+santorfo@users.noreply.github.com>
2018-12-04 21:02:40 +0000

    codigo matlab

--- src/project/MinHash.java
+++ src/project/MinHash.java
@@ -16,9 +16,37 @@ public class MinHash {
 	
 	public void insertMinHash(String iputInserMH) {
 		//Calculate the MinHash to insert on the table
+
+
+/*
+    filmes = Set{u};
+    for i=1:nhf
+        minimo = hash_function(filmes(1),i,randlist,p);
+        for t=2:countElements
+            temp = hash_function(filmes(t),i,randlist,p);
+            if temp < minimo
+                minimo = temp;
+            end
+        end
+        minhash_matrix(i,u) = minimo;
+    end
+*/
+
 	}
 
 	public void distancesMinHash(String inputDistances) {
+		/*
+		%matriz de distâncias
+dist_matrix = zeros(nhf,Nu);
+
+for u1=1:Nu
+    for u2 = 1:u1
+        dist_matrix(u1,u2) = 1 - (sum(minhash_matrix(:,u1) == minhash_matrix(:,u2)) /nhf);
+        %a distância de u1 para u2 é igual à de u2 para u1 logo:
+        dist_matrix(u2,u1) = dist_matrix(u1,u2);
+    end
+end
+		*/
 	}
 
 	public void findSimilar(String similarSearch) {
