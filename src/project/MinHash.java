3d31a3198d7a29bea3486b0aad03cbf5a8aa0766
santorfo <39157156+santorfo@users.noreply.github.com>
2018-12-06 00:44:45 +0000

    findSimilar já funciona

--- src/project/MinHash.java
+++ src/project/MinHash.java
@@ -62,7 +62,7 @@ public class MinHash {
 		
 		for(int u1=0; u1<numCols; u1++)
 		{
-			for(int u2=1; u2<numCols; u2++)
+			for(int u2=0; u2<u1; u2++)
 			{
 				double sum = 0;
 				double dist = 0;
@@ -84,39 +84,17 @@ public class MinHash {
 		this.threshold = threshold;
 		for(int u1=0; u1<numCols; u1++)
 		{
-			for(int u2=1; u2<numCols; u2++)
-			{	System.out.println(distance_matrix[u1][u2]);
-				/*
-				if(distance_matrix[u1][u2] < threshold)
-					//System.out.println(u1+" and "+u2+" are a similar pair");
-				else
-					continue;
+			for(int u2=u1+1; u2<numCols; u2++)
+			{	
+				//System.out.println(distance_matrix[u1][u2]);
+			
+				if(distance_matrix[u1][u2] < threshold && distance_matrix[u1][u2] != 0.0){
+					System.out.print((u1+1)+" and "+(u2+1)+" are a similar pair - ");
+					System.out.println(distance_matrix[u1][u2]);
+				}
 					
-				*/
+			
 			}
 		}
-	}
-
-	public void numOfSimilar() {
-		// Print the number of similar items. Saying your password it is similar to X
-		// passwords on the most XXXX common passwords which represents %%%%%. Do an if
-		// which says staying in this part of the percentage might be bad or good.
-		// Investigate this last part
-
-		/*
-		SimilarUsers= zeros(1,num_pairs);
-
-k= 1;
-    for n1= 1:Nu
-        for n2= n1+1:Nu
-            if dist_matrix(n1,n2) < threshold
-                SimilarUsers(k,:)= [n1 n2 dist_matrix(n1,n2)];
-                k= k+1;
-            end
-        end
-    end
-    
-end
-*/
 	}
 }
