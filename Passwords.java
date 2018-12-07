142bead138e8bd8b7db23181451ab5bd3343f298
Tomas Batista <34144094+tomas99batista@users.noreply.github.com>
2018-12-07 14:59:48 +0000

    paths fix

---
+++ Passwords.java
@@ -0,0 +1,42 @@
+package project;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Passwords {
+
+	public Passwords() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws IOException {
+		Scanner file_passwords = new Scanner(new File("src/project/WorstPasswords_10k.txt"));
+		Scanner file_twitter = new Scanner(new File("src/project/Twitter_Passwords.txt"));
+
+		//------------------------------------CONTADOR ESTOCASTICO--------------------------------------
+		
+		// Probability added to the ContadorEstocastico
+		new ContadorEstocastico(0.5);
+
+		while (file_passwords.hasNext()) {
+			String word = file_passwords.next();
+			// Increment the ContadorEstocastico and the count
+			ContadorEstocastico.increment();
+		}
+		while (file_twitter.hasNext()) {
+			String word = file_twitter.next();
+			// Increment the ContadorEstocastico and the count
+			ContadorEstocastico.increment();
+		}
+		// Print the number of elements added to the ContadorEstocastico and the total
+		// number of elements
+		ContadorEstocastico.numOfElements();
+		
+		
+		//------------------------------------COUNTING BLOOMFILTER--------------------------------------
+		
+	}
+
+}
---
+++ TestEstocastico.java
@@ -0,0 +1,32 @@
+package project;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class TestEstocastico {
+	public static void main(String[] args) throws FileNotFoundException {
+		// Tests to the ContadorEstocastico
+
+		// Probability added to the ContadorEstocastico
+		new ContadorEstocastico(0.5);
+
+		// Using the file u.data provided to the practical exercises
+		Scanner input = new Scanner(new File("src/project/u.data"));
+		//Scanner input = new Scanner(new File("u.data"));
+
+
+		// Made this counter to know how many words are on the file to compare to the
+		// added to ContadorEstocastico
+		int count = 0;
+		while (input.hasNext()) {
+			String word = input.next();
+			// Increment the ContadorEstocastico and the count
+			ContadorEstocastico.increment();
+			count = count + 1;
+		}
+		// Print the number of elements added to the ContadorEstocastico and the total
+		// number of elements
+		ContadorEstocastico.numOfElements();
+		System.out.println("Word count: " + count);
+	}
+}
